7febd1f0942446b3ececf8357d9286e219695f0a
thedudxo <Isaacboon@gmail.com>
2019-01-21 20:55:57 +1300

    Sounds

--- Spaceout/Assets/Project/Scripts/Player/Player.cs
+++ Spaceout/Assets/Project/Scripts/Player/Player.cs
@@ -6,6 +6,7 @@ public class Player : SpaceObject {
 
     public GameObject playerCamera;
     public GameObject background;
+    public PlayerSound playerSound;
 
     private PlayerParticles playerParticles;
     [SerializeField] private ParticleSystem engineParticles;
@@ -69,6 +70,7 @@ public class Player : SpaceObject {
         {
             engineCutout = true;
             EngineStutterParticles.Emit(100);
+            playerSound.Stutter();
         }
 
         if (engineCutout)
@@ -134,5 +136,6 @@ public class Player : SpaceObject {
         deadUI.SetActive(false);
         dead = false;
         PlayerCamera.freezeCamera = false;
+       playerSound.Respawn();
     }
 }
---
+++ Spaceout/Assets/Project/Scripts/Sound.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound{
+
+    public AudioSource Audio;
+
+    readonly float releaseTime;
+    float releasing = 0;
+    bool  playing;
+    bool randomStartTime = true;
+
+    public Sound(AudioSource Audio, float releaseTime, bool randomStartTime = false)
+    {
+        this.Audio = Audio;
+        this.releaseTime = releaseTime;
+        this.randomStartTime = randomStartTime;
+        playing = Audio.isPlaying;
+    }
+
+    public void Update()
+    {
+        if (releasing > 0)
+        {
+            releasing -= Time.deltaTime;
+            Audio.volume = releasing / releaseTime; //normalise between 0-1
+        }
+    }
+    public void Play()
+    {
+        if (randomStartTime) { Audio.time = Random.Range(0, Audio.clip.length); }
+        Audio.Play();
+        Audio.volume = 1;
+        playing = true;
+        releasing = 0;
+    }
+
+    public void Stop()
+    {
+        if(!playing) { return; } //Allready stopping sound
+        playing = false;
+        releasing = releaseTime;
+    }
+
+}
